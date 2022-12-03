c6af8389e704ea88a4374cad990571c8b33ca305
lucasdresch <49284431+lucasdresch@users.noreply.github.com>
2022-12-02 23:29:43 -0300

    Script implementations

--- InfinityRunner_2D/Assets/Scripts/SpawnPlatform.cs
+++ InfinityRunner_2D/Assets/Scripts/SpawnPlatform.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnPlatform : MonoBehaviour
-{
+public class SpawnPlatform : MonoBehaviour {
+
+    public List<GameObject> platforms = new List<GameObject>();
+    public GameObject plat;
+    public float offset;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        for(int i = 0; i < platforms.Count; i++) {
+            Instantiate(platforms[i], new Vector2(i * 30, 0), transform.rotation);
+            offset += 30f;
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        test();
     }
+    public void Recycle(GameObject plats) {
+        plats.transform.position = new Vector2(offset, 0f);
+        offset += 30f;
+    }
+    public void test() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Recycle(plat);
+        }
+    }
+
+
 }
