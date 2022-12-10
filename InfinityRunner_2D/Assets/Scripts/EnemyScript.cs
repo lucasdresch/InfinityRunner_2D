7608c0bbf12f346663fded14af9bf9152e2a717f
lucasdresch <49284431+lucasdresch@users.noreply.github.com>
2022-12-10 12:01:21 -0300

    create enemy and enemy spawn

---
+++ InfinityRunner_2D/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    private Rigidbody2D rig;
+    public float spdEnemy;
+    public float enemyLifeTime;
+    //create a type of enemies
+
+    
+    // Start is called before the first frame update
+    void Start() {
+        rig = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, enemyLifeTime);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        rig.velocity = Vector2.left * spdEnemy;
+    }
+}
---
+++ InfinityRunner_2D/Assets/Scripts/SpawnEnemiesScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemiesScript : MonoBehaviour {
+    public List<GameObject> enemiesList = new List<GameObject>();
+    private float timeCount;
+    public float spawnTime;
+    public float minPosY, maxPosY;
+    private float posY;
+
+    // Start is called before the first frame update
+    void Start() {
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timeCount += Time.deltaTime;
+        if(timeCount >= spawnTime) {
+            SpawnEnemy();
+            timeCount = 0f;
+        }
+    }
+
+    void RandomNum(float num1, float num2, float res) {
+        res = Random.Range(num1, num2);
+    }
+
+    void SpawnEnemy() {
+        RandomNum(minPosY, maxPosY, posY);
+        Instantiate(enemiesList[Random.Range(0, enemiesList.Count)], transform.position + new Vector3(0f, posY, 0f), transform.rotation);
+    }
+}
