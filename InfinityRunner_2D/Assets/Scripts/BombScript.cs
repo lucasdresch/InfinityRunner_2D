a327c7d71efb9cf2a329038d82704325fba1e9d0
lucasdresch <49284431+lucasdresch@users.noreply.github.com>
2022-12-13 03:36:17 -0300

    Create a bomb and scriptBomb
    Create a 3 bombs and scriptBomb

---
+++ InfinityRunner_2D/Assets/Scripts/BombScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour {
+
+    private Rigidbody2D rig;
+    public float forceX, forceY;
+
+    // Start is called before the first frame update
+    void Start() {
+        rig = GetComponent<Rigidbody2D>();
+        Fire();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+    void Fire() {
+        rig.AddForce(new Vector2(forceX * -1, forceY), ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        
+    }
+
+}
